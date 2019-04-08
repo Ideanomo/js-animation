30b8e36f7f29e7f582edba364bb76ee1f4af8cf6
Dan <ds23@hotmail.co.uk>
2019-04-08 09:40:52 +0100

    Add utils function to calculate mouse position

--- include/utils.js
+++ include/utils.js
@@ -1,2 +1,29 @@
 
 var utils = {};
+
+/* Calculate mouse position byoffset */
+utils.captureMouse = function(element) {
+    var mouse = {x: 0, y: 0};
+    element.addEventListener('mousemove', function(event) {
+        var x, y;
+        // Set x and y values with the event.pageX and pageY
+        // or get the document offset position of the element
+        if(event.pageX || event.pageY) {
+            x = event.pageX;
+            y = event.pageY;
+        } else {
+            x = event.clientX + document.body.scrollLeft +
+            document.documentElement.scrollLeft;
+            y = event.clientY + document.body.scrollTop +
+            document.documentElement.body.scrollTop;
+        }
+        x -= element.offsetLeft;
+        y -= element.offsetTop;
+
+        mouse.x = x;
+        mouse.y = y;
+    }, false);
+    
+    return mouse;
+
+}
