974c55302253b34e461fac32f10cdff4105c766c
Dan <ds23@hotmail.co.uk>
2019-06-07 20:09:43 +0100

    Add new utility function to keep track of current touch position

--- include/utils.js
+++ include/utils.js
@@ -1,8 +1,14 @@
 
 var utils = {};
 
-/* Calculate mouse position byoffset */
+/**
+ * Track the current mouse position, relative to an element
+ * Calculate mouse position by offset
+ * @param {HTMLElement} element
+ * @return {object} Contains properties: x, y, event
+ */
 utils.captureMouse = function(element) {
+    // Create mouse object
     var mouse = {x: 0, y: 0};
     element.addEventListener('mousemove', function(event) {
         var x, y;
@@ -23,7 +29,54 @@ utils.captureMouse = function(element) {
         mouse.x = x;
         mouse.y = y;
     }, false);
-    
-    return mouse;
 
+    // Return mouse object with x, y properties
+    return mouse;
 }
+
+/**
+ * Track the current (first) touch position, relative to an element
+ * @param {HTMLElement} element
+ * @return {object} Contains properties: x, y, isPressed, event
+ */
+utils.captureTouch = function (element) {
+    // Create touch object
+    var touch = {x: null, y: null, isPressed: false, event: null};
+
+    element.addEventListener('touchstart', function (event) {
+        touch.isPressed = true;
+        touch.event = event;
+    }, false);
+
+    element.addEventListener('touchend', function (event) {
+        touch.isPressed = false;
+        touch.x = null;
+        touch.y = null;
+        touch.event = event;
+    }, false);
+
+    element.addEventListener('touchmove', function (event) {
+        var x, y,
+        touch_event = event.touches[0]; // first touch
+
+        if (touch_event.pageX || touch_event.pageY) {
+            x = touch_event.pageX;
+            y = touch_event.pageY;
+        } else {
+            x = touch_event.clientX + document.body.scrollLeft +
+                document.documentElement.scrollLeft;
+            y = touch_event.clientY + document.body.scrollTop +
+                document.documentElement.scrollTop;
+        }
+        x -= element.offsetLeft;
+        y -= element.offsetTop;
+
+        touch.x = x;
+        touch.y = y;
+        touch.event = event;
+    }, false);
+
+    // Returns the touch object with x, y and isPressed properties
+    return touch;
+};
+
